c5db88c569b62fe380d10d287ed293fb559f7b38
Matthew Carney <matthewcarney64@gmail.com>
2021-09-30 00:40:12 +0100

    Added main extraction code (it's hella slow)

--- SC4HeightMapExtractor/Program.cs
+++ SC4HeightMapExtractor/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 using SC4Parser.Files;
 
@@ -12,6 +11,81 @@ namespace SC4HeightMapExtractor
     {
         static void Main(string[] args)
         {
+            List<string> paths = new List<string>();
+            foreach (var arg in args)
+            {
+                FileAttributes attributes = new FileAttributes();
+                try
+                {
+                    attributes = File.GetAttributes(arg);
+                }
+                catch (Exception ex) 
+                {
+                    Console.WriteLine("Could not read files at {0} ({1})",
+                        arg,
+                        ex.GetType().ToString());
+
+                    continue;
+                }
+
+                if (attributes.HasFlag(FileAttributes.Directory))
+                {
+                    paths.AddRange(Directory.GetFiles(arg, ".sc4", SearchOption.AllDirectories));
+                } 
+                else
+                {
+                    paths.Add(arg);
+                }
+            }
+
+            foreach (var path in paths)
+            {
+                SC4SaveFile file = null;
+                try
+                {
+                    file = new SC4SaveFile(path);
+
+                    var regionInfo = file.GetRegionViewSubfile();
+                    float[][] terrainData = file.GetTerrainMapSubfile().Map;
+
+                    string filename = string.Format("{0}_size-{1}x{2}_pos-{3}x{4}.csv",
+                        regionInfo.CityName,
+                        regionInfo.CitySizeX,
+                        regionInfo.CitySizeY,
+                        regionInfo.TileXLocation,
+                        regionInfo.TileYLocation);
+
+                    string fileData = "";
+                    for (int y = 0; y < regionInfo.CitySizeY; y++)
+                    {
+                        for (int x = 0; x < regionInfo.CitySizeX; x++)
+                        {
+                            fileData += terrainData[x][y];
+                            if (x != regionInfo.CitySizeX - 1)
+                            {
+                                fileData += ",";
+                            }
+                        }
+                        fileData += Environment.NewLine;
+                    }
+
+                    File.WriteAllText(filename, fileData);
+                    Console.WriteLine("Height map extracted for {0} @ {1}",
+                        Path.GetFileName(path),
+                        filename);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Can't read SC4 save files {0} ({1})",
+                        path,
+                        e.GetType().ToString());
+
+                    continue;
+                }
+
+            }
+
         }
+
     }
 }
